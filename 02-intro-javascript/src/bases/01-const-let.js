b708c7c393960ec8ba69a5d0cee5cb023327b493
Jhon-Contreras <jhon.contreras13@gmail.com>
2022-01-04 15:39:02 -0300

    bases js

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// variables y constantes 
+
+const nombre = 'Jhon';
+const apellido = 'Contreras';
+
+let valorDado = 4;
+
+
+console.log(nombre, apellido, valorDado);
+
+if( true ){
+    // variables en su ambito o scope propio 
+    let nombre = 'Juan';
+    console.log( nombre );
+}
+
+// var no debe usarse 
---
+++ 02-intro-javascript/src/bases/02-template-strings.js
@@ -0,0 +1,13 @@
+const nombre = 'Jhon';
+const apellido = 'Contreras';
+
+const nombreCompleto = `Hola, soy ${nombre} ${apellido}`;
+console.log(nombreCompleto);
+
+
+
+function getSaludo(nombre){
+    return 'hola mundo ' + nombre;
+    
+}
+console.log(`Saludo: ${getSaludo(nombre)}`);
