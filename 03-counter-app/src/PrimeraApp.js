c27e49f68e2d22f9fd59f2cb011f4be5314157c9
Jhon-Contreras <jhon.contreras13@gmail.com>
2022-01-10 14:36:43 -0300

    comunicacion entre componentes con PROPS

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -7,18 +7,18 @@
 
 // Functional components
 
-const PrimeraApp = () => {
+// props con destructuracion 
+const PrimeraApp = ( { saludo = 'Hola mundo por defecto' } ) => {
 
-    const saludo = 'Hola mundo';
+    // const saludo = 'Hola mundo';
     // const saludoObjeto = {
     //     nombre : 'JHhon',
     //     edad: 2
     // };
-
     return (
        <>
             {/* <pre> { JSON.stringify(saludoObjeto, null, 3) } </pre> */}
-            <h1> {saludo} </h1>
+            <h1> { saludo } </h1>
             <p>Soy un parrafo</p>
        </>
         // componente con Fragment (necesita importar libreria) 
