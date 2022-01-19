262ecdfda7bc7d7d2013eac2351306e237dc445e
Jhon-Contreras <jhon.contreras13@gmail.com>
2022-01-19 19:10:56 -0300

    Tests a componentes con Enzyme - primeros pasos, config

--- 03-counter-app/src/tests/PrimeraApp.test.jsx
+++ 03-counter-app/src/tests/PrimeraApp.test.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import PrimeraApp from "../PrimeraApp";
+import { shallow } from 'enzyme';
 
 describe('Pruebas en <PrimerApp />', () => {
-  test('Debe de mostrar el mensaje "hola mundo"', () => {
+//prueba con  import '@testing-library/jest-dom/extend-expect'; en setupTests.js
+
+    //   test('Debe de mostrar el mensaje "hola mundo"', () => {
     
-    const saludo = 'hola mundo';
-    const { getByText } = render(<PrimeraApp saludo={saludo}/>);
+//     const saludo = 'hola mundo';
+//     const { getByText } = render(<PrimeraApp saludo={saludo}/>);
 
-    expect(getByText(saludo)).toBeInTheDocument();
+//     expect(getByText(saludo)).toBeInTheDocument();
 
-  });
-  
+//   });
+
+    test('Debe de mostrar <PrimeraApp /> correctamente', () => {
+      const saludo = 'hola mundo';
+      const wrapper = shallow(<PrimeraApp saludo={ saludo }/>); //shallow es parecido al render()
+
+      expect( wrapper ).toMatchSnapshot();
+    });
+    
 });
 
 
