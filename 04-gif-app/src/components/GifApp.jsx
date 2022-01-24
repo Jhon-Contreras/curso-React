6120e821ad6e640a5b03d9fa178cc57379075f68
Jhon-Contreras <jhon.contreras13@gmail.com>
2022-01-24 14:35:52 -0300

    Lista ordenada de categorias

--- 04-gif-app/src/components/GifApp.jsx
+++ 04-gif-app/src/components/GifApp.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifApp = () => {
+
+    // const categories = ['Star Wars', 'Marvel', 'DC'];
+
+    const [categories, setCategories] = useState(['Star Wars Rebels', 'Clone Wars', 'The Mandalorian']);
+
+    const handleAdd = () =>{
+        // esta funcion debe agregar un nuevo elemento al arreglo, con setCategories 
+        // setCategories( [...categories, 'The book of Boba Fett'] );
+        setCategories( cats => [...cats, 'The book of Boba Fett'] );
+    }
+
     return (
         <div>
             <h2>Gif App</h2>
             <hr />
+
+            <button onClick={ handleAdd }> Agregar </button>
+
+            <ol>
+                { 
+                    categories.map( category =>{
+                       
+                        return <li key={ category }> {category} </li>
+                    }) 
+                }
+            </ol>
         </div>
     );
 };
